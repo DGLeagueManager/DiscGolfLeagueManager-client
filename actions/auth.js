cf819e0ce4e5c7c79a99bea5be819d7903e10484
antoniojcaporicci <antoniojcaporicci@gmail.com>
2017-10-16 16:45:04 -0500

    redux thunk added to axios route

--- actions/auth.js
+++ actions/auth.js
@@ -1,18 +1,12 @@
-export const login = (username, password) => {
-    return {
-        type: 'LOGIN',
-        username: username,
-        password: password
-    };
-};
+import axios from 'axios';
 
-export const logout = () => {
-    return {
-        type: 'LOGOUT'
-    };
-};
+export const signUp = (firstName, lastName, email, password) => {
+	return (dispatch) => {
+		dispatch({ type: 'REQUEST_STARTED' });
 
-export const signup = (username, password) => {
-    return (dispatch) => {
-    };
-};
+		axios.post('http://ec2-54-165-58-14.compute-1.amazonaws.com:3000/api/addPlayer', { firstName, lastName, email, password })
+			.then(response => dispatch({ type: 'REQUEST_SUCCEEDED', payload: response }))
+			.catch(error => dispatch({ type: 'REQUEST_FAILED', error: error }))    
+	};
+}
+
--- reducers/auth.js
+++ reducers/auth.js
@@ -1,35 +1,26 @@
 const defaultState = {
-  isLoggedIn: false,
-  username: '',
-  password: ''
+  isLoggedIn: false
 };
 
 export default function reducer(state = defaultState, action) {
   switch (action.type) {
-    case 'LOGIN':
+    case 'REQUEST_STARTED':
+      console.log('request started')
+      return Object.assign({}, state, {
+        isLoggedIn: false,
+      });
+    case 'REQUEST_SUCCEEDED':
       return Object.assign({}, state, {
         isLoggedIn: true,
         user: action.payload
       });
-    case 'LOGOUT':
+    case 'REQUEST_FAILED':
+      console.log('request failed')
       return Object.assign({}, state, {
         isLoggedIn: false,
-        username: '',
-        password: ''
+        error: action.error
       });
     default:
       return state;
   }
 }
-
-
-// axios.post('http://ec2-54-165-58-14.compute-1.amazonaws.com:3000/api/addPlayer', {
-//   firstName: 'Fred',
-//   lastName: 'Flintstone'
-// })
-//   .then(function (response) {
-//     console.log(response);
-//   })
-//   .catch(function (response) {
-//     console.log(response);
-//   });
