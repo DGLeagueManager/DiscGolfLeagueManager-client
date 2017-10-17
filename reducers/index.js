1eca0723543111a0ec44cbb0ca0ac49c4d3755ec
antoniojcaporicci <antoniojcaporicci@gmail.com>
2017-10-17 14:45:22 -0500

    adding score post

--- reducers/index.js
+++ reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
+import scoreCounterReducer from './scoreCounterReducer';
 
 
-const rootReducer = combineReducers({ auth });
+const rootReducer = combineReducers({ auth, scoreCounterReducer });
 export default rootReducer;
