ecad04ab944c88a0a389123d7f85cd1b28f5af97
antoniojcaporicci <antoniojcaporicci@gmail.com>
2017-10-17 18:51:21 -0500

    posting score card to the database

--- actions/auth.js
+++ actions/auth.js
@@ -5,7 +5,7 @@ export const signUp = (first_name, last_name, email, password) => {
 	return (dispatch) => {
 		dispatch({ type: 'REQUEST_STARTED' });
 
-		axios.post('http://ec2-54-165-58-14.compute-1.amazonaws.com:3000/api/addPlayer', { first_name, last_name, email, password })
+		axios.post('http://ec2-54-165-58-14.compute-1.amazonaws.com:3000/addPlayer', { first_name, last_name, email, password })
 			.then(response => dispatch({ type: 'REQUEST_SUCCEEDED', payload: response }))
 			.catch(error => dispatch({ type: 'REQUEST_FAILED', error: error }))    
 	};
--- actions/scoreCounterActions.js
+++ actions/scoreCounterActions.js
@@ -5,9 +5,10 @@ export const postScores = (scores) => {
 	return (dispatch) => {
 		dispatch({ type: 'SCORES_GETTING_POSTED'});
 
-		axios.post('http://ec2-54-165-58-14.compute-1.amazonaws.com:3000/api/addScores', { scores })
+		axios.post('http://ec2-54-165-58-14.compute-1.amazonaws.com:3000/addScores',  scores)
 			.then(response => dispatch({ type: 'SCORES_POST_SUCCEEDED', payload: response }))
 			.catch(error => dispatch({ type: 'SCORES_POST_FAILED', error: error }))    
 	};
 }
 
+//add headers
