0c23d18d9855e4d520f9203b7b1405bc70013846
antoniojcaporicci <antoniojcaporicci@gmail.com>
2017-10-21 16:37:38 -0500

    Added action and reducer for generating empty card

--- reducers/adminRoundConfigStartReducer.js
+++ reducers/adminRoundConfigStartReducer.js
@@ -1,10 +1,10 @@
 export default function reducer(state = {}, action) {
-  console.log('at the reducer', action)
   switch (action.type) {
     case "ADD_PLAYERS_TO_ROUND":
       return Object.assign({}, state, {
         amPlayers: action.payload.amPlayers,
-        proPlayers: action.payload.proPlayers
+        proPlayers: action.payload.proPlayers,
+        cards: action.payload.cards
       });
     default:
       return state;
