f9d8603e02ec26f4d2c994e356c95a551f53458b
Pete Givens <pgivens@gmail.com>
2017-10-20 19:35:12 -0500

    more cleanup

--- reducers/adminTab/NewRound.js
+++
@@ -1,9 +0,0 @@
-export default function reducer(state = {}, action) {
-  switch (action.type) {
-    case "FETCH_LEAGUE_DATA_INITIATED": {
-      // TODO
-    }
-    default:
-      return state;
-  }
-}
