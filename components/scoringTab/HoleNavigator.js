cee5c9ce390c8f48daa1895063091eac36e80190
Pete Givens <pgivens@gmail.com>
2017-10-27 18:03:53 -0500

    fixed holenavigator and integrated Finalize Score component

--- components/scoringTab/HoleNavigator.js
+++ components/scoringTab/HoleNavigator.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { TabNavigator, TabBarTop } from "react-navigation"; // 1.0.0-beta.15
 import { Text, View } from "react-native";
 import { holeDetails } from "./dummyData";
+import FinalizeScore from './FinalizeScore';
 
 class HoleNavigator extends Component {
   constructor(props) {
@@ -32,26 +33,18 @@ class HoleNavigator extends Component {
     }, {});
   }
 
-const FinalizeScoreTab = {
-  FinalizeScore: {
-    screen: FinalizeScore,
-    navigationOptions: {
-      tabBarLabel: 'FinalizeScore'
-    }
-  }
-}
-
-const HoleNavigator = TabNavigator(
-  Object.assign(generateTabs(holeDetails), FinalizeScoreTab),
-  {
-    tabBarComponent: TabBarTop,
-    tabBarPosition: "top",
-    tabBarOptions: {
-      scrollEnabled: true,
-      swipeEnabled: true,
-      showLabel: true,
-      style: {
-        backgroundColor: "red"
+  render() {
+    const TabNav = TabNavigator(
+      Object.assign(this.generateTabs(holeDetails), FinalizeScoreTab), {
+      tabBarComponent: TabBarTop,
+      tabBarPosition: "top",
+      tabBarOptions: {
+        scrollEnabled: true,
+        swipeEnabled: true,
+        showLabel: true,
+        style: {
+          backgroundColor: "red"
+        }
       }
     });
     return <TabNav />;
@@ -70,5 +63,14 @@ const SampleComponent = ({ hole, card, isScoreKeeper }) => (
     )}
   </View>
 );
+
+const FinalizeScoreTab = {
+  FinalizeScore: {
+    screen: FinalizeScore,
+    navigationOptions: {
+      tabBarLabel: "FinalizeScore"
+    }
+  }
+};
 
 export default HoleNavigator;
