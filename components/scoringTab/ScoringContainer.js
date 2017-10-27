620a0e5127c78acc17f5c8aec3d3b35b07edc674
Pete Givens <pgivens@gmail.com>
2017-10-27 12:17:54 -0500

    corrected HoleNavigator to accept card as a prop

--- components/scoringTab/ScoringContainer.js
+++ components/scoringTab/ScoringContainer.js
@@ -1,41 +1,40 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import list from './dummyData';
-import Scoring from './Scoring';
-import { getCurrentRoundData } from '../../actions/getCurrentRoundDataActions'
-
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import list from "./dummyData";
+import Scoring from "./Scoring";
+import { getCurrentRoundData } from "../../actions/getCurrentRoundDataActions";
+import HoleNavigator from "./HoleNavigator";
 
 class ScoringContainer extends Component {
   constructor(props) {
-    super(props)
-    this.state = {
-
-    }
+    super(props);
+    this.state = {};
   }
 
   componentWillMount() {
-  
-    if(this.props.currentRoundId) {
-      onGetCurrentRound(this.props.currentRoundId, this.props.playerId)
+    if (this.props.currentRoundId) {
+      onGetCurrentRound(this.props.currentRoundId, this.props.playerId);
     } else {
-      setInterval(() => {        
-        this.props.onGetCurrentRound(this.props.currentRoundId, this.props.playerId)}, 10000)
+      setInterval(() => {
+        this.props.onGetCurrentRound(
+          this.props.currentRoundId,
+          this.props.playerId
+        );
+      }, 10000);
     }
   }
 
   render() {
-    console.log('CURRENT ROUND PROPS: ', this.props)
     if (!this.props.currentRound) {
       return (
-        <View style={{ flex: 1, alignItems: 'center' }} >
+        <View style={{ flex: 1, alignItems: "center" }}>
           <Text> You are not currently in a game </Text>
         </View>
-      )
+      );
     } else {
-      return <HoleNavigator card={this.props.currentCard}/>
-      // return <Scoring />
+      return <HoleNavigator card={this.props.currentCard} />;
     }
   }
 }
@@ -46,15 +45,15 @@ const mapStateToProps = (state, ownProps) => {
     playerId: state.auth.id,
     currentRoundId: state.applicationReducer.currentRoundId,
     currentCard: state.getCurrentRoundDataReducer.currentCard
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     onGetCurrentRound: (playerId, roundId) => {
       dispatch(getCurrentRoundData(playerId, roundId));
     }
-  }
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ScoringContainer);
