70f5a01cdf24a4e4e0e45973f76dcf444fbfc501
Tristyn Leos <tristyntech@gmail.com>
2017-10-27 20:01:31 -0500

    almost fixed the admin/normal views

--- components/resultsTab/Results.js
+++ components/resultsTab/Results.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Text, StyleSheet, View } from "react-native";
 import { WeekOne, WeekTwo, WeekThree } from './TempFileDeleteMe';
 import { TabNavigator, TabBarTop } from 'react-navigation';
+import SwipeALot from 'react-native-swipe-a-lot'
 
 export default class Results extends Component {
 
@@ -15,13 +16,13 @@ export default class Results extends Component {
 
 const Tabs = TabNavigator({
   WeekOne: {
-    screen: WeekOne,
+    screen: <SwipeALot>WeekOne</SwipeALot>,
     navigationOptions: {
       tabBarLabel: 'Week 1'
     }
   },
   WeekTwo: {
-    screen: WeekTwo,
+    screen: <SwipeALot>WeekTwo</SwipeALot>,
     navigationOptions: {
       tabBarLabel: 'Week 2'
     }
