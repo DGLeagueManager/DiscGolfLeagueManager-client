99213aa9b0eb84a726f2718fd492f17e1ec8514c
antoniojcaporicci <antoniojcaporicci@gmail.com>
2017-10-31 17:16:06 -0500

    working on application logic

--- components/scoringTab/ScoringContainer.js
+++ components/scoringTab/ScoringContainer.js
@@ -7,6 +7,7 @@ import Scoring from './Scoring';
 import { getCurrentRoundData } from '../../actions/getCurrentRoundDataActions';
 import FinalizeScore from './FinalizeScore';
 import HoleNavigator from './HoleNavigator';
+import axios from 'axios';
 
 class ScoringContainer extends Component {
   constructor(props) {
@@ -14,11 +15,19 @@ class ScoringContainer extends Component {
     this.state = {};
   }
 
+  componentWillMount() {
+    if(this.props.currentRoundInProgress) {
+      axios.get('http://ec2-54-165-58-14.compute-1.amazonaws.com:3000/getPlayerRoundById/' + id)
+      .then((response) => { dispatch({ type: 'GET_LEAGUE_DATA_SUCCEEDED', payload: response })})
+    }
+  }
+
   shouldComponentUpdate(nextProps, nextState) {
     console.log('THIS DOT PROPS AND NEXT PROPS IN SCORING CONTAINER: ', this.props, nextProps)
     if (nextProps.currentRoundInProgress === false || nextProps.currenRoundInProgress === undefined) {
       return true
     } else {
+
       return false
     }
   }
@@ -52,6 +61,9 @@ const mapDispatchToProps = dispatch => {
   return {
     onGetCurrentRound: (playerId, roundId) => {
       dispatch(getCurrentRoundData(playerId, roundId));
+    },
+    onGetPlayerRoundById: (playerId) => {
+      dispatch(getPlayerRoundById(playerId))
     }
   };
 };
