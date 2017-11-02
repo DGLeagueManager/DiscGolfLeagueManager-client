a6cf37450819369aedce8fc78a9195a2ba9b4f95
Pete Givens <pgivens@gmail.com>
2017-11-02 08:52:18 -0500

    color palette

--- colorPalette.js
+++ colorPalette.js
@@ -1,12 +1,9 @@
 module.exports.palette = {
-  primary: "#32974d",
-  secondary: "#fff",
-  accent: "#0b5821",
-  primaryText: '#fff',
-  secondaryText: '#000',
-  accentText: '#32974d'
-  
-
+  primary: "#48494A",
+  secondary: "#A3A3A3",
+  background: "#292929",
+  accent: "#00D6A1",
+  text: '#fff'
 }
 // module.exports.palette = {
 //   primary: "#32974d",
