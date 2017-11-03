c93c0c0e130ec282637185c53e352433b7002914
Pete Givens <pgivens@gmail.com>
2017-11-03 10:23:58 -0500

    morning update, minor styling

--- colorPalette.js
+++ colorPalette.js
@@ -1,6 +1,6 @@
 module.exports.palette = {
   primary: "#48494A",
-  secondary: "#A3A3A3",
+  secondary: "white",
   background: "#292929",
   accent: "#00D6A1",
   accent2: "orange",
--- components/leagueTab/Info.js
+++ components/leagueTab/Info.js
@@ -6,37 +6,31 @@ import { Icon } from 'react-native-elements';
 
 export default class Info extends Component {
   render() {
-    return (
-      <ScrollView style={{backgroundColor: palette.background}}>
-        <Card
-          containerStyle={{backgroundColor: palette.secondary, borderColor: '#fff'}}
-          title='AUSTIN DISC GOLF CLUB'
-          image={{uri: 'http://eagleoutside.com/wp-content/uploads/2015/06/fairgrounds-disc-golf-course.jpg'}}>
+    return <ScrollView style={{ backgroundColor: palette.background }}>
+        <Card containerStyle={{ backgroundColor: palette.secondary }} 
+          title="AUSTIN DISC GOLF CLUB" 
+          titleStyle={{ color: palette.accent }} 
+          image={{ uri: "http://eagleoutside.com/wp-content/uploads/2015/06/fairgrounds-disc-golf-course.jpg" }}
+        >
           <Text style={{ marginBottom: 10 }}>
-            Welcome to the Austin Disc Golf Club.  Any information or changes will be posted here.  We meet Tuesday nights starting 11/12 at 6 pm, don't be afraid to invite friends!
+            Welcome to the Austin Disc Golf Club. Any information or changes
+            will be posted here. We meet Tuesday nights starting 11/12 at 6
+            pm, don't be afraid to invite friends!
           </Text>
-          <Button
-            icon={{ name: 'code' }}
-            backgroundColor='#03A9F4'
-            buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }}
-            title='Course Information Here' />
+          <Button 
+            icon={{ name: "code" }} 
+            backgroundColor={palette.accent2} 
+            buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0 }} title="Course Information Here" />
         </Card>
 
-        <Card
-          containerStyle={{ backgroundColor: palette.secondary }}
-          title='TIME CHANGE THIS COMING WEEK (11/19)'
-          titleStyle={{color: 'red'}}
-        >
-        <Icon
-          name='md-rainy'
-          type='ionicon'
-          size={30}
-          color={palette.accent}
-        />
-        <Text>Due to inclimate weather, we will postpone this Tuesday's game to Wednesday 11/20</Text>
+        <Card containerStyle={{ backgroundColor: palette.secondary }} title="TIME CHANGE THIS COMING WEEK (11/19)" titleStyle={{ color: palette.accent }}>
+          <Icon name="md-rainy" type="ionicon" size={30} color={palette.accent2} />
+          <Text>
+            Due to inclimate weather, we will postpone this Tuesday's game
+            to Wednesday 11/20
+          </Text>
         </Card>
-      </ScrollView>
-    );
+      </ScrollView>;
   }
 }
 
