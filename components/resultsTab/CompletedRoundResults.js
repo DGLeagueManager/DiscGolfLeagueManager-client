dc3db88355a3da6b7c5d8399eed5b436044c6d73
Pete Givens <pgivens@gmail.com>
2017-11-03 14:57:16 -0500

    finished Results component for presentation

--- components/resultsTab/CompletedRoundResults.js
+++ components/resultsTab/CompletedRoundResults.js
@@ -1,38 +1,31 @@
 import React from "react";
 import { Text, StyleSheet, View, ScrollView } from "react-native";
 import { Icon, List, ListItem, Divider } from "react-native-elements";
+import { palette } from '../../colorPalette';
 
 const CompletedRoundResults = ({ round }) => {
   return (
-    <ScrollView style={{ paddingTop: 20 }}>
-      <View style={{ flex: 1, flexDirection: "row" }}>
-        <Text style={styles.name}>
-          PLAYER
-        </Text>
-        <View style={{ flex: 1, flexDirection: "row" }}>
-          {!round.completed ? <Text style={styles.col}>START</Text> : null}
-          {!round.completed ? <Text style={styles.col}>THRU</Text> : null}
-          <Text style={styles.col}>ROUND</Text>
+    <ScrollView style={{ backgroundColor: palette.background }}>
+      <List containerStyle={{flex: 1}}>
+        <View style={{ flex: 1, flexDirection: "row", backgroundColor: palette.accent2, padding: 10 }}>
+          <Text style={styles.name}>
+            PLAYER
+          </Text>
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <Text style={styles.col}>ROUND</Text>
+          </View>
         </View>
-      </View>
-      <View>
-        <List containerStyle={{flex: 1}}>
-          {round.scores
-            ? Object.keys(round.scores).map(key => (
-                <ListItem
-                  key={key}
-                  title={round.scores[key].player_name}
-                  label={
-                    <Text style={styles.col}>
-                      {round.scores[key].scoreRelativeToPar}
-                    </Text>
-                  }
-                  hideChevron
-                />
-              ))
-            : null}
-        </List>
-      </View>
+        {round.scores
+          ? Object.keys(round.scores).map(key => (
+              <ListItem
+                key={key}
+                title={round.scores[key].player_name}
+                label={<Text style={{ marginRight: 60}}>{round.scores[key].scoreRelativeToPar}</Text>}
+                hideChevron
+              />
+            ))
+          : null}
+      </List>
     </ScrollView>
   );
 };
@@ -46,7 +39,6 @@ const styles = StyleSheet.create({
   col: {
     flex: 1,
     fontSize: 14,
-    justifyContent: 'space-between'
   }
 });
 
--- components/resultsTab/CurrentRoundResults.js
+++ components/resultsTab/CurrentRoundResults.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Text, StyleSheet, View, ScrollView } from "react-native";
 import { Icon, List, ListItem, Divider } from "react-native-elements";
 import { connect } from 'react-redux';
+import { palette } from '../../colorPalette';
 
 class CurrentRoundResults extends Component {
   constructor(props) {
@@ -10,41 +11,39 @@ class CurrentRoundResults extends Component {
 
   render() {
     return (
-      <ScrollView style={{ paddingTop: 20 }}>
-        <View style={{ flex: 1, flexDirection: "row" }}>
-          <Text style={styles.name}>
-            PLAYER
-          </Text>
-          <View style={{ flex: 1, flexDirection: "row" }}>
-            <Text style={styles.col}>START</Text>
-            <Text style={styles.col}>THRU</Text>
-            <Text style={styles.col}>ROUND</Text>
+      <ScrollView style={{ backgroundColor: palette.background }}>
+        <List containerStyle={{flex: 1}}>
+          <View style={{ flex: 1, flexDirection: "row", backgroundColor: palette.accent2, padding: 10 }}>
+            <Text style={styles.name}>PLAYER</Text>
+            <View style={{ flex: 1, flexDirection: "row" }}>
+              <Text style={styles.col}>START</Text>
+              <Text style={styles.col}>THRU</Text>
+              <Text style={styles.col}>ROUND</Text>
+            </View>
           </View>
-        </View>
-        <View>
-          <List>
-            {Object.keys(this.props.scores).map(key => (
-                  <ListItem
-                    key={key}
-                    title={this.props.scores[key].player_name}
+          {this.props.scores
+            ? Object.keys(this.props.scores).map(key => (
+                <ListItem
+                  key={key}
+                  title={this.props.scores[key].player_name}
                     label={
-                      <View style={{ flex: 1, flexDirection: "row" }}>
-                        <Text style={{ flex: 1, marginLeft: 10 }}>
-                          {this.props.scores[key].startingHole}
-                        </Text>
-                        <Text style={styles.col}>
-                          {this.props.scores[key].thru}
-                        </Text>
-                        <Text style={styles.col}>
-                          {this.props.scores[key].scoreRelativeToPar}
-                        </Text>
-                      </View>
-                    }
-                    hideChevron
-                  />
-                ))}
-          </List>
-        </View>
+                    <View style={{ flex: 1, flexDirection: "row" }}>
+                      <Text style={{ flex: 1, marginLeft: 10 }}>
+                        {this.props.scores[key].startingHole}
+                      </Text>
+                      <Text style={styles.col}>
+                        {this.props.scores[key].thru}
+                      </Text>
+                      <Text style={styles.col}>
+                        {this.props.scores[key].scoreRelativeToPar}
+                      </Text>
+                    </View>
+                  }
+                  hideChevron
+                />
+              ))
+            : null}
+        </List>
       </ScrollView>
     );
   };
@@ -54,16 +53,11 @@ const styles = StyleSheet.create({
   name: {
     flex: 1,
     fontSize: 14,
-    paddingLeft: 10
-  },
-  completed: {
-    flex: 3,
-    fontSize: 14,
-    paddingLeft: 10
+    paddingLeft: 20
   },
   col: {
     flex: 1,
-    fontSize: 14
+    fontSize: 14,
   }
 });
 
