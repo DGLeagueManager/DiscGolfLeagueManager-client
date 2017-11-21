73780c42c6dc64953c5c286128ef435cdf769ba7
Pete Givens <pgivens@gmail.com>
2017-11-21 15:06:34 -0600

    modified a couple of ESLint rules, linted Scoring.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
     "rules": {  
         "react/jsx-filename-extension": 0,
         "no-use-before-define": 0,
-        "linebreak-style": 0
+        "linebreak-style": 0,
+        "no-underscore-dangle": 0,
+        "react/forbid-prop-types": 0
     }
 };
