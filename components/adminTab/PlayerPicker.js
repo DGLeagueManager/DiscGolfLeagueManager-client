55c740d78eeb572d51e51750fba892dc0c9eb339
Pete Givens <pgivens@gmail.com>
2017-11-21 17:04:10 -0600

    linted PlayerPicker.js

--- components/adminTab/PlayerPicker.js
+++ components/adminTab/PlayerPicker.js
@@ -1,27 +1,44 @@
-import React, { Component } from "react";
-import { View, Text, Modal } from "react-native";
-import { Button, Divider } from "react-native-elements";
+import React from 'react';
+import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import { Button } from 'react-native-elements';
 import { palette } from '../../colorPalette';
 
-const PlayerPicker = (props) => {
-  const player = props.player;
+const PlayerPicker = ({ player, showModal, cardKey }) => {
   if (!player) {
-  return (
-    <Button
-      onPress={() => props.showModal(props.cardKey)}
-      buttonStyle={{ marginTop: 20 }}
-      backgroundColor={palette.accent}
-      title="Select Player..."
-    />
-  );
-  } else {
     return (
-      <View style={{padding: 5}}>
-        <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center', color: palette.text }}>{player.first_name + " " + player.last_name}</Text>
-      </View >
-    )
+      <Button
+        onPress={() => showModal(cardKey)}
+        buttonStyle={{ marginTop: 20 }}
+        backgroundColor={palette.accent}
+        title="Select Player..."
+      />
+    );
   }
-}
+  return (
+    <View style={{ padding: 5 }}>
+      <Text style={
+        {
+          fontSize: 20,
+          fontWeight: 'bold',
+          textAlign: 'center',
+          color: palette.text,
+        }
+      }
+      >{`${player.first_name} ${player.last_name}`}
+      </Text>
+    </View >
+  );
+};
 
+PlayerPicker.propTypes = ({
+  player: PropTypes.object,
+  showModal: PropTypes.bool,
+  cardKey: PropTypes.number.isRequired,
+});
 
+PlayerPicker.defaultProps = ({
+  player: null,
+  showModal: false,
+});
 export default PlayerPicker;
