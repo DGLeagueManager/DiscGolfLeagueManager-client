220b63bf8b34302b8ce71083688028981f61f1c1
Pete Givens <pgivens@gmail.com>
2017-11-21 16:48:23 -0600

    linted LeagueRoundInProgress.js

--- components/adminTab/LeagueRoundInProgress.js
+++ components/adminTab/LeagueRoundInProgress.js
@@ -1,44 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
-import { connect } from 'react-redux';
 import { palette } from '../../colorPalette';
 
-export default class LeagueRoundInProgress extends Component {
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Card title="League Round In Progress">
-
-          <View>
-            <Icon
-              name='account-alert'
-              type='material-community'
-              size={65}
-              color={palette.accent}
-            />
-            <Text>You will be able to start a new round after the</Text>
-            <Text style={styles.subText}>current round has ended</Text>
-          </View>
-        </Card>
+const LeagueRoundInProgress = () => (
+  <View style={styles.container}>
+    <Card title="League Round In Progress">
+      <View>
+        <Icon
+          name="account-alert"
+          type="material-community"
+          size={65}
+          color={palette.accent}
+        />
+        <Text>You will be able to start a new round after the</Text>
+        <Text style={styles.subText}>current round has ended</Text>
       </View>
-    ) 
-  }
-}
+    </Card>
+  </View>
+);
 
 const styles = StyleSheet.create({
-  subText: {marginLeft: '20%',
-   marginRight: '20%',
-   width: '60%'
- },
+  subText: {
+    marginLeft: '20%',
+    marginRight: '20%',
+    width: '60%',
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: palette.background
+    backgroundColor: palette.background,
   },
 });
 
-
-
+export default LeagueRoundInProgress;
