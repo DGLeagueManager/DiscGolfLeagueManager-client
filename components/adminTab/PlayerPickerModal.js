25ed120f10ed7c096dc58b9b342a4c89173d5b40
Pete Givens <pgivens@gmail.com>
2017-11-21 17:20:26 -0600

    linted PlayerPickerModal.js

--- components/adminTab/PlayerPickerModal.js
+++ components/adminTab/PlayerPickerModal.js
@@ -1,53 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text, TouchableHighlight, StyleSheet } from 'react-native';
 import Modal from 'react-native-modal';
 import { Button } from 'react-native-elements';
+import PropTypes from 'prop-types';
 
-
-const PlayerPickerModal = (props) => (
+const PlayerPickerModal = props => (
   <Modal
     isVisible={props.isVisible}
-    onRequestClose={() => {
-      console.log("");
-    }}
   >
     <View >
-      {props.unassignedPlayers.map(player => {
-        return (
-          <TouchableHighlight
-            onPress={value => {
-              props.handleSelectPlayer(props.unassignedPlayers.indexOf(player), props.activeCard);
-            }}
-            key={player._id}
-          >
-            <View style={styles.modalContent}>
-              <Text>{player.first_name + " " + player.last_name}</Text>
-            </View>
-          </TouchableHighlight>
-        );
-      })}
-      <Button onPress={() => props.hideModal(null)} title="Close" />
+      {props.unassignedPlayers.map(player => (
+        <TouchableHighlight
+          onPress={() => {
+            props.handleSelectPlayer(props.unassignedPlayers.indexOf(player), props.activeCard);
+          }}
+          key={player._id}
+        >
+          <View style={styles.modalContent}>
+            <Text>{`${player.first_name} ${player.last_name}`}</Text>
+          </View>
+        </TouchableHighlight>
+        ))
+      }
+      <Button onPress={() => props.hideModal()} title="Close" />
     </View>
   </Modal>
-)
+);
 
 const styles = StyleSheet.create({
 
   modalContent: {
-    backgroundColor: "white",
+    backgroundColor: 'white',
     padding: 22,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 4,
-    borderColor: "rgba(0, 0, 0, 0.1)"
-  },
-    listItem: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    fontSize: 30,
-    padding: 5,
-  }
+    borderRadius: 4,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+  },
+});
+
+PlayerPickerModal.propTypes = ({
+  isVisible: PropTypes.bool,
+  unassignedPlayers: PropTypes.array.isRequired,
+  hideModal: PropTypes.func.isRequired,
+});
+
+PlayerPickerModal.defaultProps = ({
+  isVisible: false,
 });
 
 export default PlayerPickerModal;
