0fc133ff87ad0190e732db700f4c00a39acabed4
antoniojcaporicci <antoniojcaporicci@gmail.com>
2017-11-21 13:02:36 -0700

    cleaning up errors in currentRound tab

--- components/resultsTab/CompletedRoundResults.js
+++ components/resultsTab/CompletedRoundResults.js
@@ -1,29 +1,32 @@
-import React from "react";
-import { Text, StyleSheet, View, ScrollView } from "react-native";
-import { Icon, List, ListItem, Divider } from "react-native-elements";
+import React from 'react';
+import { Text, StyleSheet, View, ScrollView } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
 import { palette } from '../../colorPalette';
 
 const CompletedRoundResults = ({ round }) => {
   return (
     <ScrollView style={{ backgroundColor: palette.background }}>
-      <List containerStyle={{flex: 1}}>
-        <View style={{ flex: 1, flexDirection: "row", backgroundColor: palette.accent2, padding: 10 }}>
+      <List containerStyle={{ flex: 1 }}>
+        <View style={{
+          flex: 1, flexDirection: 'row', backgroundColor: palette.accent2, padding: 10,
+        }}
+        >
           <Text style={styles.name}>
             PLAYER
           </Text>
-          <View style={{ flex: 1, flexDirection: "row" }}>
+          <View style={{ flex: 1, flexDirection: 'row' }}>
             <Text style={styles.col}>ROUND</Text>
           </View>
         </View>
         {round.scores
           ? Object.keys(round.scores).map(key => (
-              <ListItem
-                key={key}
-                title={round.scores[key].player_name}
-                label={<Text style={{ marginRight: 60}}>{round.scores[key].scoreRelativeToPar}</Text>}
-                hideChevron
-              />
-            ))
+            <ListItem
+              key={key}
+              title={round.scores[key].player_name}
+              label={<Text style={{ marginRight: 60 }}>{round.scores[key].scoreRelativeToPar}</Text>}
+              hideChevron
+            />
+          ))
           : null}
       </List>
     </ScrollView>
@@ -34,12 +37,12 @@ const styles = StyleSheet.create({
   name: {
     flex: 3,
     fontSize: 14,
-    paddingLeft: 20
+    paddingLeft: 20,
   },
   col: {
     flex: 1,
     fontSize: 14,
-  }
+  },
 });
 
 export default CompletedRoundResults;
--- components/socketsTab/Socket.js
+++
@@ -1,1 +0,0 @@
-//Where I will be testing
