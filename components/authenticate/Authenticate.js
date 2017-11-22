2cb4171c24be1e07ef43a3f9f002328e8cdda771
Pete Givens <pgivens@gmail.com>
2017-11-21 23:29:02 -0600

    linted Authentication components

--- components/authenticate/Authenticate.js
+++ components/authenticate/Authenticate.js
@@ -1,29 +1,37 @@
-import React, { Component } from "react";
-import SignUp from "./SignUp";
-import Login from "./Login";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import SignUp from './SignUp';
+import Login from './Login';
 import { signUp, login } from '../../actions/auth';
-
 
 class Authenticate extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      first_name: "",
-      last_name: "",
-      email: "",
-      password: "",
-      showSignUp: false
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      showSignUp: false,
     };
+
+    this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+    this.handleLastNameChange = this.handleLastNameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.userLogin = this.userLogin.bind(this);
+    this.userSignUp = this.userSignUp.bind(this);
+    this.toggleShowSignUp = this.toggleShowSignUp.bind(this);
   }
 
   handleFirstNameChange(text) {
-    this.setState({ first_name: text });
+    this.setState({ firstName: text });
   }
 
   handleLastNameChange(text) {
-    this.setState({ last_name: text });
+    this.setState({ lastName: text });
   }
 
   handleEmailChange(text) {
@@ -36,10 +44,10 @@ class Authenticate extends Component {
 
   userSignUp(e) {
     this.props.onSignUp(
-      this.state.first_name,
-      this.state.last_name,
+      this.state.firstName,
+      this.state.lastName,
       this.state.email,
-      this.state.password
+      this.state.password,
     );
     e.preventDefault();
   }
@@ -56,23 +64,24 @@ class Authenticate extends Component {
   render() {
     return this.state.showSignUp ? (
       <SignUp
-        userSignUp={this.userSignUp.bind(this)}
-        handleFirstNameChange={this.handleFirstNameChange.bind(this)}
-        handleLastNameChange={this.handleLastNameChange.bind(this)}
-        handleEmailChange={this.handleEmailChange.bind(this)}
-        handlePasswordChange={this.handlePasswordChange.bind(this)}
-        toggleShowSignUp={this.toggleShowSignUp.bind(this)}
-        firstName={this.state.first_name}
-        lastName={this.state.last_name}
+        userSignUp={this.userSignUp}
+        handleFirstNameChange={this.handleFirstNameChange}
+        handleLastNameChange={this.handleLastNameChange}
+        handleEmailChange={this.handleEmailChange}
+        handlePasswordChange={this.handlePasswordChange}
+        toggleShowSignUp={this.toggleShowSignUp}
+        firstName={this.state.firstName}
+        lastName={this.state.lastName}
         email={this.state.email}
         password={this.state.password}
+        error={this.props.error}
       />
     ) : (
       <Login
-        userLogin={this.userLogin.bind(this)}
-        handleEmailChange={this.handleEmailChange.bind(this)}
-        handlePasswordChange={this.handlePasswordChange.bind(this)}
-        toggleShowSignUp={this.toggleShowSignUp.bind(this)}
+        userLogin={this.userLogin}
+        handleEmailChange={this.handleEmailChange}
+        handlePasswordChange={this.handlePasswordChange}
+        toggleShowSignUp={this.toggleShowSignUp}
         email={this.state.email}
         password={this.state.password}
       />
@@ -80,22 +89,31 @@ class Authenticate extends Component {
   }
 }
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    // isLoggedIn: state.auth.isLoggedIn,
+const mapStateToProps = state => (
+  {
     error: state.auth.error,
-  };
-};
+  }
+);
 
-const mapDispatchToProps = dispatch => {
-  return {
-    onSignUp: (first_name, last_name, email, password) => {
-      dispatch(signUp(first_name, last_name, email, password));
+const mapDispatchToProps = dispatch => (
+  {
+    onSignUp: (firstName, lastName, email, password) => {
+      dispatch(signUp(firstName, lastName, email, password));
     },
     onLogin: (email, password) => {
       dispatch(login(email, password));
-    }
-  };
-};
+    },
+  }
+);
+
+Authenticate.propTypes = ({
+  onSignUp: PropTypes.func.isRequired,
+  onLogin: PropTypes.func.isRequired,
+  error: PropTypes.object,
+});
+
+Authenticate.defaultProps = ({
+  error: null,
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Authenticate);
--- components/authenticate/Login.js
+++ components/authenticate/Login.js
@@ -1,93 +1,109 @@
-import React, { Component } from "react";
-import { Text, View, KeyboardAvoidingView, StyleSheet, ScrollView, Alert, Image } from "react-native";
-import { FormLabel, FormInput, Button, Icon } from "react-native-elements"; // 0.17.0
-import { Constants } from "expo";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text, View, KeyboardAvoidingView, StyleSheet } from 'react-native';
+import { FormLabel, FormInput, Button } from 'react-native-elements'; // 0.17.0
+import { Constants } from 'expo';
 import { palette } from '../../colorPalette';
 
-const Login = (props) => {
-  return (
-      <KeyboardAvoidingView behavior='padding' style={styles.mainContainer}>
-        <Text style={styles.paragraph}> LOGIN </Text>
-        <View style={styles.formView}>
+const Login = props => (
+  <KeyboardAvoidingView behavior="padding" style={styles.mainContainer}>
+    <Text style={styles.paragraph}> LOGIN </Text>
+    <View style={styles.formView}>
 
-          <FormLabel>Email: </FormLabel>
-          <FormInput
-            autoCapitalize="none"
-            autoCorrect={false}
-            autoFocus={true}
-            value={props.email}
-            style={styles.input}
-            onChangeText={props.handleEmailChange}
-          />
+      <FormLabel>Email: </FormLabel>
+      <FormInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        autoFocus
+        value={props.email}
+        style={styles.input}
+        onChangeText={props.handleEmailChange}
+      />
 
-          <FormLabel>Password: </FormLabel>
-          <FormInput
-            autoCapitalize="none"
-            autoCorrect={false}
-            secureTextEntry={true}
-            value={props.password}
-            style={styles.input}
-            onChangeText={props.handlePasswordChange}
-          />
+      <FormLabel>Password: </FormLabel>
+      <FormInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+        value={props.password}
+        style={styles.input}
+        onChangeText={props.handlePasswordChange}
+      />
 
-        </View>
-          <Button
-            onPress={props.userLogin}
-            title="Submit"
-            icon={{ name: "check", buttonStyle: styles.submitButton}}
-            backgroundColor={palette.accent}
-            fontWeight={'bold'}
-            buttonStyle={styles.submitButton}
-            /* disabled={
-              props.first_name === "" ||
-              props.last_name === "" ||
-              props.email === "" ||
-              props.password === ""
-            } */
-          />
-          <Button
-            onPress={props.toggleShowSignUp}
-            buttonStyle={ styles.createButton }
-            title="Create an Account"
-          />
-          {props.error ? (
-            <Text>Email or Password not valid</Text>
-          ) : null}
-      </KeyboardAvoidingView>
-  );
-}
+    </View>
+    <Button
+      onPress={props.userLogin}
+      title="Submit"
+      icon={{ name: 'check', buttonStyle: styles.submitButton }}
+      backgroundColor={palette.accent}
+      fontWeight="bold"
+      buttonStyle={styles.submitButton}
+      /* disabled={
+        props.first_name === "" ||
+        props.last_name === "" ||
+        props.email === "" ||
+        props.password === ""
+      } */
+    />
+    <Button
+      onPress={props.toggleShowSignUp}
+      buttonStyle={styles.createButton}
+      title="Create an Account"
+    />
+    {props.error ? (
+      <Text>Email or Password not valid</Text>
+    ) : null}
+  </KeyboardAvoidingView>
+);
+
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: palette.primary
+    backgroundColor: palette.primary,
   },
   submitButton: {
     borderRadius: 10,
-    margin: 10
+    margin: 10,
   },
   createButton: {
     borderRadius: 10,
-    margin: 10
+    margin: 10,
   },
   paragraph: {
     margin: 24,
     fontSize: 14,
-    fontWeight: "bold",
-    textAlign: "center",
-    color: palette.text
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: palette.text,
   },
   input: {
     width: 240,
     height: 60,
     fontSize: 30,
-    color: palette.text
+    color: palette.text,
   },
   formView: {
     backgroundColor: palette.primary,
-  }
+  },
+});
+
+Login.propTypes = ({
+  email: PropTypes.string,
+  password: PropTypes.string,
+  handleEmailChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  userLogin: PropTypes.func.isRequired,
+  toggleShowSignUp: PropTypes.func.isRequired,
+  error: PropTypes.object,
+});
+
+Login.defaultProps = ({
+  email: '',
+  password: '',
+  error: null,
 });
 
 export default Login;
