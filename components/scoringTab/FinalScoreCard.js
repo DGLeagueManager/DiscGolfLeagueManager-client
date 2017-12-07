651aa50e380c106ba814cd07f5de11b26b4e4894
antoniojcaporicci <antoniojcaporicci@gmail.com>
2017-12-07 12:02:46 -0700

    fixed header in admin stack

--- components/scoringTab/FinalScoreCard.js
+++ components/scoringTab/FinalScoreCard.js
@@ -10,31 +10,30 @@ export default class ScoreKeeperCard extends Component {
   }
 
   render() {
-    console.log("SCORE KEEPER CARD props: scores: ", this.props.scores);
-    return <Card title={null}>
-        <View style={styles.titleContainer}>
-          <View>
-            <Text 
-              style={styles.title}
-              numberOfLines={1}
-              ellipsizeMode={'tail'}
-            >
-              {(this.props.player.first_name +
-                " " +
-                this.props.player.last_name
-              ).toUpperCase()}
-            </Text>
-          </View>
-          <View>
-            <Text style={styles.score}>
-              {this.props.scores.scoreRelativeToPar} ({this.props.scores.totalStrokes})
-            </Text>
-          </View>
+    return (
+    <Card title={null}>
+      <View style={styles.titleContainer}>
+        <View>
+          <Text
+            style={styles.title}
+            numberOfLines={1}
+            ellipsizeMode={'tail'}
+          >
+            {(this.props.player.first_name +
+              " " +
+              this.props.player.last_name
+            ).toUpperCase()}
+          </Text>
         </View>
-
-        <Button raised icon={{ name: "assignment" }} title="Review Hole Scores" buttonStyle={{ backgroundColor: palette.accent2 }} />
-
-      </Card>;
+        <View>
+          <Text style={styles.score}>
+            {this.props.scores.scoreRelativeToPar} ({this.props.scores.totalStrokes})
+            </Text>
+        </View>
+      </View>
+      <Button raised icon={{ name: "assignment" }} title="Review Hole Scores" buttonStyle={{ backgroundColor: palette.accent2 }} />
+    </Card>
+    );
   }
 }
 
